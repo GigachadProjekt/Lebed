a7f1a38b9dce1fd25e6a2a7e1a1bb63df4d747e3
GigachadProjekt <127248452+GigachadProjekt@users.noreply.github.com>
2023-03-12 21:09:16 +0300

    Update Lebed_Configurator.UE4.js

--- Lebed_Configurator.UE4.js
+++ Lebed_Configurator.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Lebed_Configurator.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/GigachadProjekt/Lebed/3be920fafe15074c5c329a82265c6f900d1dd866/Lebed_Configurator.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
